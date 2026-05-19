2c19e1632c09d3b00863d572dc6c690237f01026
andrea <andreamariefrauchette@gmail.com>
2026-05-19 19:32:20 +0000

    first commiy

--- week1/day1/daily_exercises/gold.py
+++ week1/day1/daily_exercises/gold.py
@@ -2,18 +2,6 @@
 #     Ask the user for their birthdate (specify the format, for example: DD/MM/YYYY).
 #     Display a little cake as seen below:
 
-#        ___iiiii___
-#       |:H:a:p:p:y:|
-#     __|___________|__
-#    |^^^^^^^^^^^^^^^^^|
-#    |:B:i:r:t:h:d:a:y:|
-#    |                 |
-#    ~~~~~~~~~~~~~~~~~~~
-
-# The number of candles on the cake should be the last number of the users age, if they are 53, then add 3 candles.
-
-# Bonus : If they were born on a leap year, display two cakes !
-
 birthdate = input("Enter your birthdate (DD/MM/YYYY): ")
 day, month, year = map(int, birthdate.split('/'))
 age = 2023 - year
