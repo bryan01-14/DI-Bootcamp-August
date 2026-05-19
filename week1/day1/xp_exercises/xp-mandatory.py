d7efc01548721052837ee894f27c46556d25af3f
andrea <andreamariefrauchette@gmail.com>
2026-05-19 19:56:38 +0000

    first commiy

--- week1/day1/xp_exercises/xp-mandatory.py
+++ week1/day1/xp_exercises/xp-mandatory.py
@@ -77,6 +77,7 @@ else:
 # Write code that will ask the user for their height in centimeters.
 # If they are over 145 cm, print a message that states they are tall enough to ride.
 # If they are not tall enough, print a message that says they need to grow some more to ride.
+
 height = int(input("What is your height in centimeters? "))
 if height > 145:
     print("You are tall enough to ride.")
