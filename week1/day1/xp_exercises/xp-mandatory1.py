27998ed29cac19a714da72159400eb7916ced93e
andrea <andreamariefrauchette@gmail.com>
2026-06-28 00:53:23 +0000

    first commit

--- week1/day1/xp_exercises/xp-mandatory1.py
+++ week1/day1/xp_exercises/xp-mandatory1.py
@@ -6,13 +6,17 @@
 #     Create a program that prints a list of multiples of the number until the list length reaches length.
 
 
-number = int(input("Enter a number: "))
-length = int(input("Enter a length: "))
-liste = []
+try:
+    number = int(input("Enter a number: "))
+    length = int(input("Enter a length: "))
+except ValueError:
+    print("Please enter valid whole numbers for both the number and the length.")
+else:
+    liste = []
 
-for i in range(1, length + 1):
-    liste.append(number * i)
-print(liste)
+    for i in range(1, length + 1):
+        liste.append(number * i)
+    print(liste)
 
 
 # Challenge 2
@@ -21,11 +25,12 @@ print(liste)
 
 
 string = input("Enter a string: ")
-new_string = ""
+new_chars = []
 
 for i in range(len(string)):
     if i == 0 or string[i] != string[i - 1]:
-        new_string += string[i]
+        new_chars.append(string[i])
+new_string = ''.join(new_chars)
 print(new_string)
 
 
